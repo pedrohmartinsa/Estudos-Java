4eefcf4660b5788f202d8cd2b38dbb7c79b9fb32
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-11-26 21:16:37 -0300

    minhas primeiras aplicações de classes com java, usando criação de variáveis, concatenação e casting

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Alô Mundo");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+
+        boolean incluidoNoPlano = true;
+
+        double media = (9.8 + 6.3 + 8.0) / 3;
+
+        String sinopse = """
+                Filme: Top Gun: Maverick
+                Filmo com galá dos anos 80
+                Muito bom!
+                Ano de lançamento: %d
+                Incluido no plano ? %b
+                Media de nota: %.2f
+                """.formatted(anoDeLancamento, incluidoNoPlano ,media);
+        System.out.println(sinopse);
+
+        double dado = 5.5;
+        int dado2 = (int) dado;
+        System.out.println(dado2);
     }
+
 }
